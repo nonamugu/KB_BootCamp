49f25de44616847690704139f6bce65607ad00ac
jaehyun <0908jhk@naver.com>
2026-03-25 17:35:57 +0900

    16일차 Vue 활용한 todolist 간단 프로젝트 커밋

---
+++ 01_WEB/03_Vue/chapter08/slot-test/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import NamedSlotTest from './components/NamedSlotTest.vue';
+import NoSlotTest from './components/NoSlotTest.vue';
+import SlotTest from './components/SlotTest.vue';
+export default {
+  name: 'App',
+  components: { NoSlotTest, SlotTest, NamedSlotTest },
+};
+</script>
+
+<template>
+  <div>
+    <NoSlotTest />
+    <SlotTest />
+    <NamedSlotTest />
+  </div>
+</template>
+
+<style scoped></style>
