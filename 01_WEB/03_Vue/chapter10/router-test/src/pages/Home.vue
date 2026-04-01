17b1273cc58a369e71206f77c1c4e0ab49d75d01
jaehyun <0908jhk@naver.com>
2026-04-01 14:40:10 +0900

    26.04.01 1주차 과제 제출 실습 커밋

---
+++ 01_WEB/03_Vue/chapter10/router-test/src/pages/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="card card-body">
+    <h2 class="text-center">Home</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Home',
+};
+</script>
---
+++ 01_WEB/03_Vue/chapter10/router-test/src/pages/Members.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="card card-body">
+    <h2 class="text-center">Members</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Members',
+};
+</script>
